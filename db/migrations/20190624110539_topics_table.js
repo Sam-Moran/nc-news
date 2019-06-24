143ae811a8fe3097aaf580e6b9dedf3ff3787d26
sammoran@gmail.com <sammoran@gmail.com>
2019-06-24 11:18:21 +0100

    migrate latest function made

---
+++ db/migrations/20190624110539_topics_table.js
@@ -0,0 +1,10 @@
+exports.up = function(connection, Promise) {
+	return connection.schema.createTable("topics", topicsTable => {
+		topicsTable.string("description").notNullable();
+		topicsTable
+			.string("slug")
+			.notNullable()
+			.primary();
+	});
+};
+exports.down = function(connection, Promise) {};
