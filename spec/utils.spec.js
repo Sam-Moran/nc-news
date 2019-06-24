1135da010dae3a311541f1b0efbcdaafb6c2487c
sammoran@gmail.com <sammoran@gmail.com>
2019-06-24 14:30:22 +0100

    4th test created and passes

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -33,6 +33,49 @@ describe("formatDate", () => {
 		];
 		expect(actual).to.eql(final);
 	});
+
+	it("returns an array of objects with the time formatted with the rest of the data included", () => {
+		const data = [
+			{
+				title: "Running a Node App",
+				topic: "coding",
+				author: "jessjelly",
+				body:
+					"This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+				created_at: 1471522072389
+			},
+			{
+				title:
+					"The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+				topic: "coding",
+				author: "jessjelly",
+				body:
+					"Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.",
+				created_at: 1500584273256
+			}
+		];
+		const actual = formatDate(data);
+		const expected = [
+			{
+				title: "Running a Node App",
+				topic: "coding",
+				author: "jessjelly",
+				body:
+					"This is part two of a series on how to get up and running with Systemd and Node.js. This part dives deeper into how to successfully run your app with systemd long-term, and how to set it up in a production environment.",
+				created_at: new Date("2016-08-18T12:07:52.389Z")
+			},
+			{
+				title:
+					"The Rise Of Thinking Machines: How IBM's Watson Takes On The World",
+				topic: "coding",
+				author: "jessjelly",
+				body:
+					"Many people know Watson as the IBM-developed cognitive super computer that won the Jeopardy! gameshow in 2011. In truth, Watson is not actually a computer but a set of algorithms and APIs, and since winning TV fame (and a $1 million prize) IBM has put it to use tackling tough problems in every industry from healthcare to finance. Most recently, IBM has announced several new partnerships which aim to take things even further, and put its cognitive capabilities to use solving a whole new range of problems around the world.",
+				created_at: new Date("2017-07-20T20:57:53.256Z")
+			}
+		];
+		expect(actual).to.eql(expected);
+	});
 });
 
 describe("makeRefObj", () => {});
