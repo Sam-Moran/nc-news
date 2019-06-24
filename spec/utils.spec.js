585f2da61de46aac3146404a646fb5dc6cd9ac50
sammoran@gmail.com <sammoran@gmail.com>
2019-06-24 14:25:19 +0100

    third test formatted correctly

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -9,9 +9,9 @@ describe("formatDate", () => {
 		expect(actual).to.eql(expected);
 	});
 	it("returns a single time to the current date", () => {
-		const time = 1471522072389;
-		const data = [{ time }];
-		const actual = formatDate(data);
+		//const time = 1471522072389;
+		const data = { created_at: 1471522072389 };
+		const actual = formatDate([data]);
 		const created_at = new Date(1471522072389);
 		const expected = [{ created_at }];
 		expect(actual).to.eql(expected);
@@ -23,10 +23,14 @@ describe("formatDate", () => {
 			{ created_at: 1500659650346 }
 		];
 		const actual = formatDate(data);
-		const expected1 = new Date(1471522072389);
-		const expected2 = new Date(1500584273256);
-		const expected3 = new Date(1500659650346);
-		const final = [{ expected1 }, { expected2 }, { expected3 }];
+		const expected1 = new Date("2016-08-18T12:07:52.389Z");
+		const expected2 = new Date("2017-07-20T20:57:53.256Z");
+		const expected3 = new Date("2017-07-21T17:54:10.346Z");
+		const final = [
+			{ created_at: expected1 },
+			{ created_at: expected2 },
+			{ created_at: expected3 }
+		];
 		expect(actual).to.eql(final);
 	});
 });
