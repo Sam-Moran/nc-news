f245bdabd5d17e189f07d12a7f6c6144216f5f98
sammoran@gmail.com <sammoran@gmail.com>
2019-06-24 14:06:14 +0100

    third test created for multiple objects

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -16,8 +16,18 @@ describe("formatDate", () => {
 		const expected = [{ created_at }];
 		expect(actual).to.eql(expected);
 	});
-	it('returns an array of objects with time formatted', () => {
-		cons
+	it("returns an array of objects with time formatted", () => {
+		const data = [
+			{ created_at: 1471522072389 },
+			{ created_at: 1500584273256 },
+			{ created_at: 1500659650346 }
+		];
+		const actual = formatDate(data);
+		const expected1 = new Date(1471522072389);
+		const expected2 = new Date(1500584273256);
+		const expected3 = new Date(1500659650346);
+		const final = [{ expected1 }, { expected2 }, { expected3 }];
+		expect(actual).to.eql(final);
 	});
 });
 
