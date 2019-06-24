acfce7471c4fe5316891aee4f5c896da6ea33742
sammoran@gmail.com <sammoran@gmail.com>
2019-06-24 11:27:04 +0100

    migration made for users

---
+++ db/migrations/20190624112152_users_table.js
@@ -0,0 +1,14 @@
+exports.up = function(connection, Promise) {
+	return connection.schema.createTable("users", usersTable => {
+		usersTable
+			.string("username")
+			.notNullable()
+			.primary();
+		usersTable.string("avatar_url");
+		usersTable.string("name").notNullable();
+	});
+};
+
+exports.down = function(connection, Promise) {
+	return connection.schema.dropTable("users");
+};
