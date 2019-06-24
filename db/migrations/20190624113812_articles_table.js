7ffff119ff899d99342bfb1a05f1c74f67c3f577
sammoran@gmail.com <sammoran@gmail.com>
2019-06-24 16:19:24 +0100

    added timestamp that i missed

--- db/migrations/20190624113812_articles_table.js
+++ db/migrations/20190624113812_articles_table.js
@@ -1,11 +1,12 @@
 exports.up = function(connection, Promise) {
 	return connection.schema.createTable("articles", articlesTable => {
 		articlesTable.increments("article_id").primary();
-		articlesTable.string("title", 20).notNullable();
+		articlesTable.string("title").notNullable();
 		articlesTable.text("body").notNullable();
 		articlesTable.integer("votes").defaultTo(0);
 		articlesTable.string("topic").references("topics.slug");
 		articlesTable.string("author").references("users.username");
+		articlesTable.timestamp("created_at").defaultTo(connection.fn.now());
 	});
 };
 
