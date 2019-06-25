21b39e57144b1bf4d00933d53b14f94eedd7656c
sammoran@gmail.com <sammoran@gmail.com>
2019-06-25 17:00:39 +0100

    util changed to no longer mutate

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const apiRouter = require("./routers/api-router");
 app.use(express.json());
 
 app.use("/api", apiRouter);
-app.all("/*", (req, res, next) => res.status(404).send("Route not found"));
+app.all("/*", (req, res, next) =>
+	res.status(404).send({ msg: "Route not found" })
+);
 
 app.use((err, req, res, next) => {
 	if (err.status) res.status(err.status).send({ msg: err.msg });
 	else next(err);
+});
+app.use((err, req, res, next) => {
+	if (err.code === "22P02") {
+		res.status(400).send({ msg: "Bad request" });
+	}
+	next(err);
 });
 
 app.use((err, req, res, next) => {
