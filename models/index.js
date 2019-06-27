22be0df21de4410c7210d96b41b1289f3ce72fc1
sammoran@gmail.com <sammoran@gmail.com>
2019-06-27 15:03:21 +0100

    checkExists function created

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const checkExists = (value, table, column) => {
+	return connection
+		.select("*")
+		.from(table)
+		.where(column, value)
+		.then(rows => {
+			if (rows.length === 0) {
+				return false;
+			}
+			return true;
+		});
+};
+
+module.exports = { checkExists };
