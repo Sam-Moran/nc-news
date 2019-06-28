0af4679ab538aa91606d12cf8189375d811d48ed
sammoran@gmail.com <sammoran@gmail.com>
2019-06-28 09:50:54 +0100

    adding endpoints

--- routers/api-router.js
+++ routers/api-router.js
@@ -4,11 +4,17 @@ const { usersRouter } = require("./users-router.js");
 const { articlesRouter } = require("./articles-router");
 const { commentsRouter } = require("./comments-router.js");
 const { methodNotAllowed } = require("../errors/errorhandling.js");
+const { endpoints } = require("../endpoints.json");
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.use("/users", usersRouter);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
-apiRouter.route("/*").all(methodNotAllowed);
+apiRouter
+	.route("/*")
+	.all(methodNotAllowed)
+	.get((req, res) => {
+		res.status(200).send({ endPoints });
+	});
 
 module.exports = apiRouter;
